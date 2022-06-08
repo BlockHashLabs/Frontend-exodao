dbdc936c215b888ad7383ad941099c05feb1d12f
bowalesamuel <bowale.samuel@lmu.edu.ng>
2022-06-08 06:57:45 +0100

    eslint issues main branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
   env: {
     node: true,
-    jest: true,
-    browser: true,
+    jest: false,
+    browser: false,
   },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
     ecmaFeatures: {
-      jsx: true,
+      jsx: false,
     },
   },
-  extends: [
-    "plugin:prettier/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-  ],
+  extends: [],
+  // extends: [
+  //   "plugin:prettier/recommended",
+  //   "plugin:react-hooks/recommended",
+  //   "plugin:@typescript-eslint/recommended",
+  //   "plugin:@typescript-eslint/eslint-recommended",
+  // ],
   plugins: ["@typescript-eslint", "simple-import-sort", "unused-imports"],
   rules: {
     "prettier/prettier": ["error"],
@@ -34,8 +35,8 @@ module.exports = {
     "no-nested-ternary": "off",
     "no-restricted-syntax": "off",
     "no-plusplus": "off",
-    "simple-import-sort/imports": "error",
-    "unused-imports/no-unused-imports": "error",
+    // "simple-import-sort/imports": "error",
+    "unused-imports/no-unused-imports": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/ban-ts-comment": "off",
@@ -43,8 +44,8 @@ module.exports = {
   },
   ignorePatterns: ["build", "node_modules"],
   globals: {
-    React: true,
-    JSX: true,
+    React: false,
+    JSX: false,
   },
   overrides: [
     {
